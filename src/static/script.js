b6698eaf3a0e9e9591e80f316040de995670cd1b
unknown <amine.mekki@mines-ales.org>
2023-06-25 23:14:24 +0200

    fix question/upload file problem

--- src/static/script.js
+++ src/static/script.js
@@ -6,12 +6,11 @@ const questionInput = document.getElementById("question");
 const fileInput = document.getElementById("file-input");
 const errorMessage = document.getElementById("error-message");
 
+let question = ''; // A variable to store the question
+
 // Add an event listener to the submit button
 submitButton.addEventListener("click", async (event) => {
     event.preventDefault(); // Prevent default form submission behavior
-
-    // Get the user's question
-    const question = questionInput.value;
 
     // Validate the form inputs
     if (!isFormValid()) {
@@ -19,25 +18,15 @@ submitButton.addEventListener("click", async (event) => {
         return;
     }
 
-    // Send a POST request to the /get_answer endpoint
-    const response = await fetch("/get_answer", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ question })
-    });
-
-    // Get the response data
-    const data = await response.json();
-
-    // Display the answer
-    const answerElement = document.getElementById("answer");
-    answerElement.textContent = data.answer;
+    // If a file has already been uploaded, send the question
+    if (fileInput.value !== '') {
+        await sendQuestion();
+    }
 });
 
 errorMessage.style.textAlign = "center";
 errorMessage.style.color = "red";
+
 // Add an event listener to the file input
 fileInput.addEventListener("change", async () => {
     // Get the uploaded file
@@ -62,10 +51,18 @@ fileInput.addEventListener("change", async () => {
     // Get the response data
     const data = await response.json();
     console.log(data); // Optional: Log the response data
+
+    // If a question has already been entered, send it
+    if (question !== '') {
+        await sendQuestion();
+    }
 });
 
-// Add event listeners for input changes
-questionInput.addEventListener("input", validateForm);
+// Add an event listener for the question input field
+questionInput.addEventListener("input", () => {
+    question = questionInput.value;
+    validateForm();
+});
 
 // Function to validate the form
 function validateForm() {
@@ -87,4 +84,23 @@ function isFormValid() {
     const fileValue = fileInput.value;
 
     return questionValue.trim() !== "" && fileValue !== "";
+}
+
+// Function to send a question
+async function sendQuestion() {
+    // Send a POST request to the /get_answer endpoint
+    const response = await fetch("/get_answer", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ question })
+    });
+
+    // Get the response data
+    const data = await response.json();
+
+    // Display the answer
+    const answerElement = document.getElementById("answer");
+    answerElement.textContent = data.answer;
 }
