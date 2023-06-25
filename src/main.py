1039d669487adfc9231531876566b0581d45b4d2
unknown <amine.mekki@mines-ales.org>
2023-06-25 23:13:39 +0200

    clean code and optimize it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request, UploadFile, File
+from fastapi import FastAPI, Request, UploadFile, HTTPException, Depends, File
+
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+
 
 from pydantic import BaseModel
 from functools import lru_cache
@@ -17,6 +19,13 @@ templates = Jinja2Templates(directory="./templates")
 
 class Query(BaseModel):
     question: str
+
+class VectorStore:
+    def __init__(self):
+        self.store = None
+
+vector_store = VectorStore()
+
 
 # Using LRU cache to store the precomputed results of expensive functions
 @lru_cache(maxsize=None)
@@ -40,30 +49,86 @@ def get_vector_store(text: str) -> object:
 
 # Background task to initialize the vector store
 executor = ThreadPoolExecutor(max_workers=1)
-vector_store = None  # Variable to store the vector store
 
 @app.get("/")
 def home(request: Request):
+    
+    """
+    The home page of the web app.
+    
+    Parameters:
+    ----------
+    
+    request : Request
+        The request object.
+    
+    Returns:
+    -------
+    response : Response
+    
+    """
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/about")
 def about(request: Request):
+    
+    """
+    The about page of the web app.
+    
+    Parameters:
+    ----------
+    
+    request : Request
+        The request object.
+    
+    Returns:
+    -------
+    response : Response
+    
+    """
     return templates.TemplateResponse("about.html", {"request": request})
 
 @app.get("/query")
 async def query(request: Request):
+    """
+    The query page of the web app.
+
+    Parameters:
+    ----------
+    request : Request
+        The request object.
+
+    Returns:
+    -------
+    response : Response
+    
+    """
     return templates.TemplateResponse("query.html", {"request": request})
 
 @app.post("/upload_file")
-async def upload_file(file: UploadFile = File(...)):
-    global vector_store  # Use the global variable
+async def upload_file(file: UploadFile = File(...)) -> dict[str, str]:
+    """
+    Upload the file and create the vector store from it.
+    
+    Parameters:
+    ----------
+    file : UploadFile  
+        The file that will uploaded from the web to create the vector store from.
+
+    Returns:
+    -------
+    response : Response
+        The response containing the filename.
+        
+    """
     contents = await file.read()
-    text = contents.decode("utf-8")  # Convert bytes to string
-    vector_store = get_vector_store(text)  # Update the vector store with the text
+    text = contents.decode("utf-8")
+    vector_store.store = get_vector_store(text)
     return {"filename": file.filename}
 
+
 @app.post("/get_answer")
-def get_answer_endpoint(query: Query):
+def get_answer_endpoint(query: Query) -> dict[str, str]:
     """
     Get the answer to the query from the most similar chunks.
 
@@ -77,6 +142,8 @@ def get_answer_endpoint(query: Query):
     response : Response
         The HTTP response containing the answer.
     """
-    query_similar_chunks = chunks_similarity_research(vector_store, query.question, 3)
+    if vector_store.store is None:
+        raise HTTPException(status_code=400, detail="No file uploaded yet")
+    query_similar_chunks = chunks_similarity_research(vector_store.store, query.question, 3)
     answer = get_answer(query.question, query_similar_chunks)
     return {"answer": answer}
